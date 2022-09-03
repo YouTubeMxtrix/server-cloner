d4dd3f43c49f5a90a5e8e280ded86b3d3efc5b45
YouTubeMxtrix <85259405+YouTubeMxtrix@users.noreply.github.com>
2022-09-03 11:11:49 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 ################################## Configuration ##################################
-input_guild_id = "921127052642512957"  # The ID of the server that you're copying
-output_guild_id = "926585375206539325"  # The ID of the server that you're pasting onto
-token = "ACCOUNT_TOKEN"  # <-- Account token. (user)
+input_guild_id = "865427998206197780"  # The ID of the server that you're copying
+output_guild_id = "1015640210681368606"  # The ID of the server that you're pasting onto
+token = "ODY1NjY5OTI5NDgyODQ2MjM4.GSX8iZ.w-n7V_Z906I77wukZImaYW_oOWA43HYj-F-p6Y"  # <-- Account token. (user)
 clear_server = True # Whether to clear the server you're pasting to or not (To delete old channels, roles, etc)
 windows = True # Whether you're on windows or not (for titling)
 ###################################################################################
